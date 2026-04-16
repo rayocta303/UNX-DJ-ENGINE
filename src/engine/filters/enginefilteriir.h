7bf1ee5ac5f3cb157103d6b941e81519cfb03fe9
Cemas Dek <46717801+rayocta303@users.noreply.github.com>
2026-04-16 11:15:37 +0700

    fix(android): Make setCoefs2 variadic to handle complex crossover initialization

--- src/engine/filters/enginefilteriir.h
+++ src/engine/filters/enginefilteriir.h
@@ -12,7 +12,9 @@ public:
     void process(CSAMPLE* pInOut, const std::size_t bufferSize) override {}
     
     virtual void setCoefs(const char* spec, int specLen, unx::audio::SampleRate sampleRate, double centerFreq) {}
-    virtual void setCoefs2(unx::audio::SampleRate sampleRate, double centerFreq, double Q) {}
+    template<typename... Args>
+    void setCoefs2(Args... args) {}
+
 
     
 protected:
